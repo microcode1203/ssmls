e113585f1819658e78c1ddf425376da10b99d23b
microcode1203 <jidc1203@gmail.com>
2026-04-18 10:29:48 +0800

    fix seed import name mismatch

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const { Server } = require('socket.io');
 const { testConnection }  = require('./config/database');
 const { runMigrations }   = require('./config/migrate');
 const { runMigrationsV2 } = require('./config/migrate_v2');
-const { seedDatabase }    = require('./config/seed');
+const { seed: seedDatabase } = require('./config/seed');
 // Socket.io - safely load
 let initSocket = () => {};
 try {
